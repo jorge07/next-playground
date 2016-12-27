d85ef67bc47bbd04872ba35ec5830bcc93bdc3f2
Jorge Arco <jorge.arcoma@gmail.com>
2016-12-27 18:33:50 +0100

    Added title

--- components/head/index.js
+++ components/head/index.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 export default () => (
     <header>
         <Head>
+            <title>Hola que ase</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
         </Head>
     </header>
